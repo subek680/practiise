fcd2f66bb88fa1aff0704874d650f4c4838a19b2
Subek Singh Khokhar <subek@Subeks-MacBook-Pro.local>
2023-04-14 16:51:11 +0530

    debounce and throttle

--- src/App.js
+++ src/App.js
@@ -1,31 +1,46 @@
 import styles from './App.module.css';
 
 function App() {
-  let name = {
-    firstName: "Subek",
-    lastName: "Singh",
-  };
-  const printFullName = function (home, ghar) {
-    console.log(this.firstName + " " + this.lastName + " " + home + " " + ghar );
+
+  function dumm(xyz, okoko, e) {
+    console.log('debounce chl gya' + xyz + ' ' + okoko + '   ' + e);
   }
-  let name2 = {
-    firstName: "Harkamal",
-    lastName: "Singh",
-  };
 
-  printFullName.call(name, "buttar", "A4");  // call method
+  const onChange = debounce(dumm, 3000);
 
-  printFullName.apply(name2, ["melbourne", "A4"]);  // apply method
-
-  const method = printFullName.bind(name, "roorkee", "F-'138");  // bind method
-  method();
-  const a = 100;
-  {
-    const a=10;
-    console.log(a);
+  function debounce(fn, d) {
+    let timer;
+    return function() {
+      let context = this;
+      clearInterval(timer);
+      timer = setTimeout(()=>{
+        fn.apply(context,['hello', 5, 'lplpl'])
+        // fn('hello', 5, e)
+      }, d)
+    }
   }
-  console.log(a);
- return <div className={styles.header}>Harry</div>
+
+  function thro(e) {
+    console.log('throttle' + ' ' + e)
+  }
+
+  const onChangeThrottle = throttle(thro, 3000);
+
+  function throttle(fn, d) {
+    let flag = true;
+    return function() {
+      let context = this;
+       if(flag) {
+        fn.apply(context, ['plplp'])
+        flag = false;
+        setTimeout(()=> {
+          flag = true;
+        }, d)
+       }
+    }
+  }
+
+ return <div className={styles.header}><div>debounce</div><input onChange={(e) => {onChange()}}></input><div>throttle</div><input onChange={() => {onChangeThrottle()}}></input></div>
 }
 
 export default App;
