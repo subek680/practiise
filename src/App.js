56a3bebd71b79fe261252076fab1370d01c12dcf
Subek Singh Khokhar <subek@Subeks-MacBook-Pro.local>
2023-04-21 17:21:02 +0530

    promises

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
 import styles from './App.module.css';
+import React, {useState} from 'react';
+import FirstFile from './FirstFile';
 
 function App() {
+  const [state,setState] = useState(false);
   let name = {
     firstName: "Subek",
     lastName: "Singh",
   };
-  const printFullName = function (home, ghar) {
-    console.log(this.firstName + " " + this.lastName + " " + home + " " + ghar );
+  function onClick() {
+    setState(true)
   }
-  let name2 = {
-    firstName: "Harkamal",
-    lastName: "Singh",
-  };
+  const promise = new Promise(function(resolve,reject){
+    setTimeout(()=> {console.log('jjjjjjkfdkdfkdkdk'); resolve('jjjjjjkfdkdfkdkdk999999999');},2000);
+    setTimeout(()=> {reject('mmmm');},10000);
+  })
 
-  printFullName.call(name, "buttar", "A4");  // call method
-
-  printFullName.apply(name2, ["melbourne", "A4"]);  // apply method
-
-  const method = printFullName.bind(name, "roorkee", "F-'138");  // bind method
-  method();
-  const a = 100;
-  {
-    const a=10;
-    console.log(a);
+  async function xyz() {
+    return await Promise.resolve('kookokoko');
   }
-  console.log(a);
- return <div className={styles.header}>Harry</div>
+
+  Promise.race([promise, xyz()]).then(value => console.log(value,'value'))
+ return <div className={styles.header}>Harry {state}<button onClick={onClick}>Button</button><FirstFile/></div>
 }
 
 export default App;
